6fca0947c5fb8b2d8add198d62c5abd1e188db13
josefdclxvi <45645153+josefdclxvi@users.noreply.github.com>
2025-04-10 22:50:45 +0800

    test: update test cases

--- index.test.js
+++ index.test.js
@@ -1,8 +1,15 @@
 import { describe, expect, it } from 'vitest'
 
+import { someRandomFunction, kemerotin } from '.'
+
 describe('suite name', () => {
   it('foo', () => {
-    expect(1 + 1).toEqual(2)
-    expect(true).to.be.true
+    expect(someRandomFunction(1, 1)).toEqual(2);
+    expect(someRandomFunction(2, 2)).toEqual(4);
+  })
+
+  it('bar', () => {
+    expect(kemerotin(1, 1)).toEqual(1);
+    expect(kemerotin(1000, 0)).toEqual(0);
   })
 })
