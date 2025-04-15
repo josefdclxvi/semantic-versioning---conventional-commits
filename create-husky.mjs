c143b4770feb3e53ec770509ef39a5c05aaab424
josefdclxvi <45645153+josefdclxvi@users.noreply.github.com>
2025-04-15 12:43:25 +0800

    refactor: update spaces, semi colons in config files and restructure the project

--- create-husky.mjs
+++ create-husky.mjs
@@ -1,37 +1,37 @@
-import { promises as fs } from 'fs'
-import path from 'path'
+import { promises as fs } from 'fs';
+import path from 'path';
 
 // Skip if in production or CI environment
 if (process.env.NODE_ENV === 'production' || process.env.CI === 'true') {
-  process.exit(0)
+  process.exit(0);
 }
 
-const husky = (await import('husky')).default
-husky()
+const husky = (await import('husky')).default;
+husky();
 
 // Use the current working directory (the root of the project)
-const rootDir = process.cwd()
+const rootDir = process.cwd();
 
 // Define the path for the .husky folder in the root directory
-const huskyDir = path.join(rootDir, '.husky')
-const underscoreDir = path.join(huskyDir, '_')
+const huskyDir = path.join(rootDir, '.husky');
+const underscoreDir = path.join(huskyDir, '_');
 
 // Define paths for the commit-msg and pre-commit hooks
-const commitMsgHookPath = path.join(huskyDir, 'commit-msg')
-const preCommitHookPath = path.join(huskyDir, 'pre-commit')
+const commitMsgHookPath = path.join(huskyDir, 'commit-msg');
+const preCommitHookPath = path.join(huskyDir, 'pre-commit');
 
 // Ensure the .husky directory exists
 try {
-  await fs.access(huskyDir)
+  await fs.access(huskyDir);
 } catch {
-  await fs.mkdir(huskyDir)
-  console.log('✅ .husky directory created')
+  await fs.mkdir(huskyDir);
+  console.log('✅ .husky directory created');
 }
 
 // Check if .husky/commit-msg exists, if not, create it
 try {
-  await fs.access(commitMsgHookPath)
-  console.log('✅ commit-msg hook already exists')
+  await fs.access(commitMsgHookPath);
+  console.log('✅ commit-msg hook already exists');
 } catch {
   await fs.writeFile(
     commitMsgHookPath,
@@ -39,15 +39,15 @@ try {
 npx --no-install commitlint --edit "$1"
 
 echo "✅ commit-msg hook ran"`
-  )
-  await fs.chmod(commitMsgHookPath, 0o755)
-  console.log('✅ commit-msg hook created')
+  );
+  await fs.chmod(commitMsgHookPath, 0o755);
+  console.log('✅ commit-msg hook created');
 }
 
 // Check if .husky/pre-commit exists, if not, create it
 try {
-  await fs.access(preCommitHookPath)
-  console.log('✅ pre-commit hook already exists')
+  await fs.access(preCommitHookPath);
+  console.log('✅ pre-commit hook already exists');
 } catch {
   await fs.writeFile(
     preCommitHookPath,
@@ -58,7 +58,7 @@ npm test
 #npx tsc --noEmit
 
 echo "✅ pre-commit hook ran"`
-  )
-  await fs.chmod(preCommitHookPath, 0o755)
-  console.log('✅ pre-commit hook created')
+  );
+  await fs.chmod(preCommitHookPath, 0o755);
+  console.log('✅ pre-commit hook created');
 }
--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,15 +1,15 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it } from 'vitest';
 
-import { someRandomFunction, kemerotin } from '.'
+import { someRandomFunction, kemerotin } from '../..';
 
 describe('suite name', () => {
   it('foo', () => {
     expect(someRandomFunction(1, 1)).toEqual(2);
     expect(someRandomFunction(2, 2)).toEqual(4);
-  })
+  });
 
   it('bar', () => {
     expect(kemerotin(1, 1)).toEqual(1);
     expect(kemerotin(1000, 0)).toEqual(0);
-  })
-})
+  });
+});
